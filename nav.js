8fc5b18a3dcf380c64e3c71d5f2a7e55630e0e8b
HaniaAtta <149865201+HaniaAtta@users.noreply.github.com>
2024-01-02 20:47:56 +0500

    Initial commit

---
+++ nav.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+       if ($(this).scrollTop() > 100) {
+         $('.navbar').addClass('solid-navbar');
+       } else {
+         $('.navbar').removeClass('solid-navbar');
+       }
+    });
+   });
